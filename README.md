03db75d33b8c9b9f9bcfbcdb52ceff28233a0fb1
Bernardngari <bernardngari.ke@gmail.com>
2022-05-15 16:10:19 +0300

    Add a README file to the project

---
+++ README.md
@@ -0,0 +1,14 @@
+# CODERED COMPUTER CLUB JAVASCRIPT TEST
+
+## Author
+Bernard Ngari
+
+## Description
+This is a website where members of the Codered computer club will take Javascript test to eveluate their proficiency in the language.
+
+## How to access site.
+The site is accessible via a web browser on this[link](https://bernardngari.github.io/week-3-IP/)
+
+## SetUp Instructions
+
+You need a browser and a working internet connection to use this website.
--- js/index.js
+++ js/index.js
@@ -1,49 +1,60 @@
 /** @format */
 
-function score() {
-	var marks = 0;
-	var quiz1 = document.test.quiz1.value;
-	var quiz2 = document.test.quiz2.value;
-	var quiz3 = document.test.quiz3.value;
-	var quiz4 = document.test.quiz4.value;
-	var quiz5 = document.test.quiz5.value;
-	var quiz6 = document.test.quiz6.value;
-	var quiz7 = document.test.quiz7.value;
-	var quiz8 = document.test.quiz8.value;
-	var quiz9 = document.test.quiz9.value;
-	var quiz10 = document.test.quiz10.value;
-
-	if (quiz1 == "Javascript") {
-		marks += 10;
-	}
-	if (quiz2 == "Raw data") {
-		marks += 10;
-	}
-	if (quiz3 == "Assignment operator") {
-		marks += 10;
-	}
-	if (quiz4 == "Script") {
-		marks += 10;
-	}
-	if (quiz5 == "False") {
-		marks += 10;
-	}
-	if (quiz6 == "Dialling a function") {
-		marks += 10;
-	}
-	if (quiz7 == "Value x") {
-		marks += 10;
-	}
-	if (quiz8 == "Javascript") {
-		marks += 10;
-	}
-	if (quiz9 == "Quotes enclosing the value Scania") {
-		marks += 10;
-	}
-	if (quiz10 == "style") {
-		marks += 10;
+function score ()
+{
+  var marks = 0;
+  var quiz1 = document.test.quiz1.value;
+  var quiz2 = document.test.quiz2.value;
+  var quiz3 = document.test.quiz3.value;
+  var quiz4 = document.test.quiz4.value;
+  var quiz5 = document.test.quiz5.value;
+  var quiz6 = document.test.quiz6.value;
+  var quiz7 = document.test.quiz7.value;
+  var quiz8 = document.test.quiz8.value;
+  var quiz9 = document.test.quiz9.value;
+  var quiz10 = document.test.quiz10.value;
+  var output=document.getElementById("output")
+  if ( quiz1 == "Javascript" )
+  {
+    marks += 10;
+  }
+  if ( quiz2 == "Raw data" )
+  {
+    marks += 10;
+  }
+  if ( quiz3 == "Assignment operator" )
+  {
+    marks += 10;
+  }
+  if ( quiz4 == "Script" )
+  {
+    marks += 10;
+  }
+  if ( quiz5 == "False" )
+  {
+    marks += 10;
+  }
+  if ( quiz6 == "Dialling a function" )
+  {
+    marks += 10;
+  }
+  if ( quiz7 == "Value x" )
+  {
+    marks += 10;
+  }
+  if ( quiz8 == "Javascript" )
+  {
+    marks += 10;
+  }
+  if ( quiz9 == "Quotes enclosing the value Scania" )
+  {
+    marks += 10;
+  }
+  if ( quiz10 == "style" )
+  {
+    marks += 10;
   };
+  output.textContent=marks
+   
 
-alert(marks);
 }
-
